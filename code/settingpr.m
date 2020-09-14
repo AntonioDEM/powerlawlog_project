88e35370084549bbc71c94eed9d05e51c33fff00
Antonio <iperstatica@gmail.com>
2020-09-14 20:52:11 +0200

    Update settingpr.m

--- code/settingpr.m
+++ code/settingpr.m
@@ -5,7 +5,7 @@
 %                       \\   /                          %     
 %                        \\ /                           %     
 %                         \/                            %     
-%	©Antonio Demarcus - 2019/2020                       %	 
+%	©Antonio Demarcus - 2019/2020                   %	 
 %    email : antonio.demarcus@gmail.com             	%	 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -33,14 +33,14 @@ CStruct.Interpreter = 'tex';
 CStruct.WindowStyle = 'modal';
 CStruct.FontSize = 12;
 nota1 = ['\fontsize{10} \rm Seleziona la cartella path'];
-nota2 = ['\fontsize{10} \rm Copiare il file di dati in formato xls nella cartella "data_xls", digitare "1" e invio, altrimenti se presente, digitare "2" e invio'];
+nota2 = ['\fontsize{10} \rm Copiare il file di dati in formato xls nella cartella "data xls", digitare "1" e invio, altrimenti se presente, digitare "2" e invio'];
 
 nota3 = ['\fontsize{10} \rm Avvia il file xlstomat per convertire il file xls'];
 
 h=msgbox(nota1,'Selection','help',CStruct);
 uiwait(h)
 
-%-- seleziona la cartella che diventerèa la path del prgetto
+%-- seleziona la cartella che diventerˆ la path del prgetto
 folder = uigetdir();
 textLabel = sprintf('la path del progetto è: %s', folder);
 
@@ -63,7 +63,7 @@ savepath;
 h=msgbox(nota2,'Selection','help',CStruct);
 uiwait(h)
 
-n = input('Vuoi copiare il file xls nella cartella data_mat?  ')
+n = input('Vuoi copiare il file data.xls nella cartella data xls?  ')
 
 switch n
     case 1
